7b32f8c1c16dee493251e1a35ee49ec6822235fd
Rostyslav <r.kovalenko.it@gmail.com>
2024-05-21 11:22:02 +0200

    Possibility to change Screens in GUI

--- gui.go
+++ gui.go
@@ -3,13 +3,14 @@ package gui
 import (
 	"context"
 
-	tl "github.com/grupawp/termloop"
 	"github.com/google/uuid"
+	tl "github.com/grupawp/termloop"
 )
 
 type GUI struct {
 	game      *tl.Game
 	drawables map[uuid.UUID][]tl.Drawable
+	screens   map[string]*tl.Screen
 }
 
 // NewGUI returns a new GUI instance.
@@ -19,16 +20,18 @@ func NewGUI(debug bool) *GUI {
 	game := tl.NewGame()
 	game.Screen().SetFps(60)
 	game.SetDebugOn(debug)
-
+	screens := make(map[string]*tl.Screen)
+	screens["init"] = game.Screen()
 	d := &GUI{
 		game:      game,
 		drawables: make(map[uuid.UUID][]tl.Drawable),
+		screens:   screens,
 	}
 
 	return d
 }
 
-// Start displays the GUI and blocks until endKey is pressed or 
+// Start displays the GUI and blocks until endKey is pressed or
 // context is done. If endKey is nil it defaults to Ctrl+C.
 func (g *GUI) Start(ctx context.Context, endKey *tl.Key) {
 	if endKey != nil {
@@ -43,6 +46,33 @@ func (g *GUI) Draw(d Drawable) {
 	for _, di := range g.drawables[d.ID()] {
 		g.game.Screen().AddEntity(di)
 	}
+}
+
+func (g *GUI) NewScreen(name string) {
+	screen := tl.NewScreen()
+	screen.SetFps(60)
+	g.screens[name] = tl.NewScreen()
+}
+
+func (g *GUI) RemoveScreen(name string) {
+	delete(g.screens, name)
+}
+
+func (g *GUI) SetScreen(name string) bool {
+	screen, ok := g.screens[name]
+	if !ok {
+		return false
+	}
+	g.game.SetScreen(screen)
+	return true
+}
+
+func (g *GUI) ScreenNames() []string {
+	names := make([]string, 0)
+	for name := range g.screens {
+		names = append(names, name)
+	}
+	return names
 }
 
 // Remove removes the given Drawable from the screen.
